db2602af9e6b85f545568e6ebcdbaaa068daff10
umeshkumar <umesh.se1988@gmail.com>
2025-07-14 22:12:55 +0530

    Demo Phone Pay App1

---
+++ Phone-Pay-app/src/test/java/com/starttohkar/PhonePay_app/PhonePayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starttohkar.PhonePay_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonePayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
